06d97f9053108a390cc14d4d966ec096c746a869
evanndev <evann.de-bailliencourt@isen.yncrea.com>
2020-04-22 13:36:00 +0200

    add eureka + external config

--- microservice_video/microservice-video/src/main/java/com/livefox/video/VideoApplication.java
+++ microservice_video/microservice-video/src/main/java/com/livefox/video/VideoApplication.java
@@ -2,8 +2,12 @@ package com.livefox.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class VideoApplication {
 
 	public static void main(String[] args) {
